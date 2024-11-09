6a1c28a66a1507467b314f9167add44d1b2ac7b4
DemigodKS <mankok222@gmail.com>
2024-11-09 21:10:48 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,26 +1,33 @@
 
+
 def add_everything_up(a, b):
 
     try:
             d = a + b
-            print(d)
+            return d
     except TypeError as exc:
             c = str(a) + str(b)
-            print(c)
+            return c
 
 
 print('Пример № 1')
 print('-----------')
-add_everything_up(5, 5.543)
+print(add_everything_up(5, 5.54))
 print('')
 print('Пример № 2')
 print('-----------')
-add_everything_up('ads', 5)
-add_everything_up(17, 'hello')
+print(add_everything_up('ads', 5))
+print(add_everything_up(17, 'hello'))
 print()
 print('Пример № 3')
 print('-----------')
-add_everything_up('ads', 5.543)
+print(add_everything_up('ads', 5.54))
+print(add_everything_up(3.17, '45'))
+
+
+
+
+
 add_everything_up(3.17, '45')
 
 
